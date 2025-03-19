65d2dc88f7302e2cc635969882577675c1943f6c
Alybit <alybit@alybit.dev>
2025-03-19 20:50:04 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from mcstatus import JavaServer, motd
+from typing import Annotated, Literal
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/api-2/{server}/{port}/{class2}/{class3}")
+async def players(server: str, port: str, class2: str, class3: str):
+    server = JavaServer.lookup(f"{server}:{port}")
+    status = server.status()
+    if class2 == "players":
+        if class3 == "online":
+            return {"response": status.players.online}
+        elif class3 == "max":
+            return {"response": status.players.max}
+    elif class2 == "motd":
+        return {"response": status.motd}
+
+@app.get("/api-1/{server}/{port}/{class2}")
+async def players(server: str, port: str, class2: str):
+    server = JavaServer.lookup(f"{server}:{port}")
+    status = server.status()
+    if class2 == "motd":
+        return {"response": status.motd}
